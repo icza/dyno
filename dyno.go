b99d93e0262ca5939f71b565f5b675449dfc4ded
Andras Belicza <iczaaa@gmail.com>
2017-12-28 11:11:50 +0100

    Add ConvertMapI2MapS

--- dyno.go
+++ dyno.go
@@ -358,3 +358,43 @@ func Append(v interface{}, value interface{}, path ...interface{}) error {
 	// Must set the new slice value:
 	return Set(v, append(s, value), path...)
 }
+
+// ConvertMapI2MapS walks the given dynamic object recursively, and
+// converts maps with interface{} key type to maps with string key type.
+// This function comes handy if you want to marshal a dynamic object into
+// JSON where maps with interface{} key type are not allowed.
+//
+// Recursion is implemented into values of the following types:
+//   -map[interface{}]interface{}
+//   -map[string]interface{}
+//   -[]interface{}
+//
+// When converting map[interface{}]interface{} to map[string]interface{},
+// fmt.Sprint() with default formatting is used to convert the key to a string key.
+func ConvertMapI2MapS(v interface{}) interface{} {
+	switch x := v.(type) {
+	case map[interface{}]interface{}:
+		m := map[string]interface{}{}
+		for k, v2 := range x {
+			switch k2 := k.(type) {
+			case string: // Fast check if it's already a string
+				m[k2] = ConvertMapI2MapS(v2)
+			default:
+				m[fmt.Sprint(k)] = ConvertMapI2MapS(v2)
+			}
+		}
+		v = m
+
+	case []interface{}:
+		for i, v2 := range x {
+			x[i] = ConvertMapI2MapS(v2)
+		}
+
+	case map[string]interface{}:
+		for k, v2 := range x {
+			x[k] = ConvertMapI2MapS(v2)
+		}
+	}
+
+	return v
+}
